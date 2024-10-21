b5d39cdf6f29a88349a079410effe621fe20fee2
Niko-P <polyakov_n.v@mail.ru>
2024-10-21 13:58:34 +0600

    Change in the sixth commit

--- Lesson6/lesson_6_task_3.py
+++ Lesson6/lesson_6_task_3.py
@@ -7,7 +7,7 @@ from selenium.webdriver.support import expected_conditions as EC
 
 driver = webdriver.Chrome(
     service=ChromeService(ChromeDriverManager().install())
-    )
+)
 
 # Установка явного ожидания
 wait = WebDriverWait(driver, 40, 0.1)
@@ -20,12 +20,21 @@ driver.get(
 # Ожидание, пока текст "Done!" не появится на странице
 wait.until(EC.text_to_be_present_in_element((By.ID, "text"), 'Done!'))
 
-# Ожидание загрузки всех изображений
-wait.until(EC.visibility_of_all_elements_located((By.TAG_NAME, 'img')))
+# Ожидание появления хотя бы одного изображения
+wait.until(EC.presence_of_all_elements_located((By.TAG_NAME, 'img')))
+
+# Проверка количества загруженных изображений
+images = driver.find_elements(By.TAG_NAME, 'img')
+print(f"Количество загруженных изображений: {len(images)}")
+
+# Если изображений меньше 4, ждем еще немного
+while len(images) < 4:
+    images = driver.find_elements(By.TAG_NAME, 'img')
 
 # Получение значения атрибута src у 3-й картинки
-image_src = driver.find_elements(By.TAG_NAME, 'img')[2].get_attribute('src')
+image_src = images[3].get_attribute('src')
 
+# Вывод ссылки на третью картинку
 print(image_src)
 
 # Закрытие браузера
